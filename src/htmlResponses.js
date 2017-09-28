a26433f21edda80285cb741d2c846e74cf748628
Hawkon Gustavsson <h.gustavsson96@gmail.com>
2017-09-28 14:59:33 -0400

    Implemented nodemon and babel watch.

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,13 +1,24 @@
 const fs = require('fs'); // pull in the file system module
 
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+// const index = fs.readFileSync(`${__dirname}/../hosted/client.html`);
 
 const getIndex = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
-  response.end();
+  fs.readFile(`${__dirname}/../hosted/client.html`, (err, data) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+  });
+};
+
+const getBundle = (request, response) => {
+  fs.readFile(`${__dirname}/../hosted/bundle.js`, (err, data) => {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.write(data);
+    response.end();
+  });
 };
 
 module.exports = {
   getIndex,
+  getBundle,
 };
